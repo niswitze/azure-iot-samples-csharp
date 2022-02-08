6299622eff6c53c2426ecf40ccb5d59a90097b73
niswitze <niswitze@microsoft.com>
2022-02-08 09:29:38 -0600

    Adding sample for X509 certificate authentication for file upload with IoT Hub

---
+++ iot-hub/Samples/device/FileUploadX509Sample/Parameters.cs
@@ -0,0 +1,50 @@
+﻿using CommandLine;
+
+namespace Microsoft.Azure.Devices.Client.Samples
+{
+    /// <summary>
+    /// Parameters for the application.
+    /// </summary>
+    internal class Parameters
+    {
+
+        [Option(
+            't',
+            "TransportType",
+            Default = TransportType.Mqtt,
+            Required = false,
+            HelpText = "The transport to use to communicate with the IoT Hub. Possible values include Mqtt, Mqtt_WebSocket_Only, Mqtt_Tcp_Only, Amqp, Amqp_WebSocket_Only, Amqp_Tcp_only, and Http1.")]
+        public TransportType TransportType { get; set; }
+
+        [Option(
+            'x',
+            "CertificatePath",
+            Required = true,
+            HelpText = "Relative file path to pfx file.")]
+        public string CertificatePath { get; set; }
+
+        [Option(
+            'p',
+            "CertificatePassword",
+            Required = true,
+            HelpText = "Pasword to read certificate data.")]
+        public string CertificatePassword { get; set; }
+
+        [Option(
+            'd',
+            "DeviceId",
+            Required = true,
+            HelpText = "Device ID in IoT Hub.")]
+        public string DeviceId { get; set; }
+
+
+        [Option(
+            'i',
+            "IoTHubHostName",
+            Required = true,
+            HelpText = "IoT Hub host name.")]
+        public string IoTHubHostName { get; set; }
+
+
+    }
+}
---
+++ iot-hub/Samples/device/FileUploadX509Sample/Program.cs
@@ -0,0 +1,89 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using CommandLine;
+using System;
+using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
+using System.IO;
+
+namespace Microsoft.Azure.Devices.Client.Samples
+{
+    /// <summary>
+    /// This sample requires an IoT Hub linked to a storage account container.
+    /// Find instructions to configure a hub at <see href="https://docs.microsoft.com/en-us/azure/iot-hub/iot-hub-configure-file-upload"/>.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// A sample to illustrate how to upload files from a device using certificate authentication.
+        /// </summary>
+        /// <param name="args">
+        /// Run with `--help` to see a list of required and optional parameters.
+        /// </param>
+        /// <returns></returns>
+        public static async Task<int> Main(string[] args)
+        {
+            // Parse application parameters
+            Parameters parameters = null;
+            ParserResult<Parameters> result = Parser.Default.ParseArguments<Parameters>(args)
+                .WithParsed(parsedParams =>
+                {
+                    parameters = parsedParams;
+                })
+                .WithNotParsed(errors =>
+                {
+                    Environment.Exit(1);
+                });
+
+            X509Certificate2 certificate = LoadProvisioningCertificate(parameters);
+
+            var authMethod = new DeviceAuthenticationWithX509Certificate(parameters.DeviceId, certificate);
+
+            Console.WriteLine($"Connecting to {parameters.IoTHubHostName} using Device ID {parameters.DeviceId}");
+
+            using var deviceClient = DeviceClient.Create(parameters.IoTHubHostName, authMethod, parameters.TransportType);
+
+            var sample = new FileUploadSample(deviceClient);
+            await sample.RunSampleAsync();
+
+            await deviceClient.CloseAsync();
+
+            Console.WriteLine("Done.");
+            return 0;
+        }
+
+        private static X509Certificate2 LoadProvisioningCertificate(Parameters parameters)
+        {
+            var certificateCollection = new X509Certificate2Collection();
+            certificateCollection.Import(
+                parameters.CertificatePath,
+                parameters.CertificatePassword,
+                X509KeyStorageFlags.UserKeySet);
+
+            X509Certificate2 certificate = null;
+
+            foreach (X509Certificate2 element in certificateCollection)
+            {
+                Console.WriteLine($"Found certificate: {element?.Thumbprint} {element?.Subject}; PrivateKey: {element?.HasPrivateKey}");
+                if (certificate == null && element.HasPrivateKey)
+                {
+                    certificate = element;
+                }
+                else
+                {
+                    element.Dispose();
+                }
+            }
+
+            if (certificate == null)
+            {
+                throw new FileNotFoundException($"{parameters.CertificatePath} did not contain any certificate with a private key.");
+            }
+
+            Console.WriteLine($"Using certificate {certificate.Thumbprint} {certificate.Subject}");
+
+            return certificate;
+        }
+    }
+}
